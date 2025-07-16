287843ef19ed21a7a3ebdea3564542cb922c7a88
Maidamsheetyvivek <vivekmaidamsheety@gmail.com>
2025-07-16 15:48:35 +0530

    Create Demo3.java

---
+++ Demo3.java
@@ -0,0 +1,8 @@
+public class EcommerceLogin {
+
+	public static void main(String[] args) {
+		System.out.println("UserName");
+		System.out.println("Password");
+		System.out.println("Click on Submit button");
+		
+	}
