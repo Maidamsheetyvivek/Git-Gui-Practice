bfd35ac1ceafa1e5c34f7c18b1ac6e236e388571
Maidamsheetyvivek <vivekmaidamsheety@gmail.com>
2025-07-16 15:49:22 +0530

    Create Demo4.java

---
+++ Demo4.java
@@ -0,0 +1,8 @@
+public class AirLinesLogin {
+
+	public static void main(String[] args) {
+		System.out.println("UserName");
+		System.out.println("Password");
+		System.out.println("Click on Submit button");
+		
+	}
